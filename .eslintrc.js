0ce69c24471798c88eab7fb5c1f2075d08a091ac
Shai Gerbi <ShaiGerbi@gmail.com>
2022-10-14 11:32:28 +0300

    Add new rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,18 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['warn'],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    'no-var': ['error'],
+    'no-multi-spaces': ['error'],
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+    'vue/attributes-order': ['error'],
+    'vue/no-multi-spaces': ['error'],
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2],
+    'vue/order-in-components': ['error']
   }
 }
